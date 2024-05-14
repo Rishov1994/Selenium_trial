40fdbb60e3bff1977008d252dbbde52e4b3aaeae
rishov.bhattacharjee <rishov_bhattacharjee@comcast.com>
2024-05-14 10:12:48 +0530

    First Commit

---
+++ Selenium1/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //System.setProperty("webdriver.chrome.driver", "C:\\Users\\rishov.bhattacharjee\\chromedriver-win64\\chromedriver.exe");
+        //WebDriver driver = new WebDriver();
+        //driver.get();
+        //driver
+
+    }
+
+}
